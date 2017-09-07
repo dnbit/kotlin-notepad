90012849fb8a2f2f0c63f77784a41f278320832d
Aaron Sarazan <aaron@levelmoney.com>
2017-09-07 12:49:19 -0700

    start database conversion

---
+++ app/src/main/java/com/udacity/notepad/data/DataStore.kt
@@ -0,0 +1,21 @@
+package com.udacity.notepad.data
+
+import android.content.Context
+import org.jetbrains.anko.doAsync
+
+object DataStore {
+
+    lateinit var notes: NoteDatabase
+
+    fun init(context: Context) {
+        notes = NoteDatabase(context)
+    }
+
+    fun execute(runnable: Runnable) {
+        execute { runnable.run() }
+    }
+
+    fun execute(fn: () -> Unit) {
+        doAsync { fn() }
+    }
+}
